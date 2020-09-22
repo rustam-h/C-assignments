9481520300a291d5a98146b51c8a1c4bdbcaf515
rustam-h <rustam.habibullayev.std@bhos.edu.az>
2020-09-23 00:08:02 +0400

    second update

---
+++ swap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void swap(int a, int b){
+	
+	printf("Before the swap:\n");
+	printf("%d, %d\n", a, b);
+
+	int temp = a;
+	a = b;
+	b = temp;
+	printf("After the swap:\n");
+	printf("%d, %d", a, b);
+	
+}
+
+int main(){
+	
+	int a, b;
+	a = 10;
+	b = 15;
+	swap(a, b);
+	return 0;
+}
+
+
