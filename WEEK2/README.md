9822296571c9fe9b76836325aadde006c72e8258
rustam-h <rustam.habibullayev.std@bhos.edu.az>
2020-09-28 21:56:54 +0400

    Initial commit

--- WEEK2/README.md
+++ WEEK2/README.md
@@ -1,1 +1,1 @@
-# C-assignments
+This function changes the values of 2 variables and prints them.
--- WEEK2/swap.c
+++ WEEK2/swap.c
@@ -19,6 +19,7 @@ int main(){
 	int a, b;
 	a = 10;
 	b = 15;
+	printf("%d\n", a*b);
 	swap(a, b);
 	return 0;
 }
